265ce7ab84073bac837950fac7cd6951ac544d63
whaiderBU <35708641+whaiderBU@users.noreply.github.com>
2018-02-21 12:32:46 -0500

    Create script3.py

---
+++ Code-Review/script3.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+import glob
+from code import get_all_tweets
+
+#This script checks if an improper twitter handle passes in the sample code
+
+os.system("  get_all_tweets('"@brekenhandlexapmle1z123"') " )
+    if return
+    break 
+    print ('Improper handle error')
+
+
